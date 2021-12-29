fb10bcb642a53e8fc917284eefc4074a22e28679
DESKTOP-FBBPA4P\USER <beichelberger0@gmail.com>
2021-12-29 16:26:03 +1000

    Add convenient wrappers for filtering bbs data by station and species code. Only plot trend line if more than 10 data points.

--- R/species_counts.R
+++ R/species_counts.R
@@ -20,21 +20,28 @@ islandwide_counts <- function(bbs_dat) {
 #' Create plot for individual bird.
 #'
 #' @param bird String for Species_Code found in bdat.
-#' @param bdat BBS dataframe with data for 'bird' Species_Code. May have more as well, intended to be passed through lapply in plot_counts.
+#' @param bbs_dat BBS dataframe with data for 'bird' Species_Code. May have more as well, intended to be passed through lapply in plot_counts.
 #'
 #' @return ggplot of counts over time for bird.
 #'
-plot_bird_count <- function(bird, bdat) {
+plot_bird_count <- function(bird, bbs_dat) {
+  # Get the specific bird
+  bdat <- bbs_dat %>% filter(.data$Species_Code == bird)
+  # Make basic plot
   plt <- bdat %>%
-         filter(.data$Species_Code == bird) %>%
          ggplot(aes(x = .data$YrQtr, y = .data$Count)) +
          geom_point() +
          geom_line() +
-         geom_smooth(color = "red", method = "loess", formula = y ~ x) +
          labs(title = bird)
+  # If more than 10 counts available, add a loess trend line
+  if(nrow(bdat) > 10) {
+    plt <- plt + geom_smooth(color = "red", method = "loess", formula = y ~ x)
+  }
+
+  plt
 }
 
-#' Create plots
+#' Create plots for multiple bird species. If no species specified, plot for all available.
 #'
 #' @param bbs_dat Dataframe with BBS data.
 #' @param birds Character vector with bird species codes.
@@ -49,5 +56,30 @@ plot_counts <- function(bbs_dat, birds = c()) {
   }
 
   # apply plot function across all species
-  lapply(birds, plot_bird_count, bdat = bbs_dat)
+  lapply(birds, plot_bird_count, bbs_dat = bbs_dat)
 }
+
+#' Convenient wrapper for filtering stations in BBS data.
+#'
+#' @param bbs_dat Dataframe with BBS data.
+#' @param active_stations Character or numeric vector with stations to retain.
+#'
+#' @return Dataframe filtered to only include active stations.
+#' @export
+#'
+filter_stations <- function(bbs_dat, active_stations) {
+  bbs_dat %>% filter(.data$Station %in% active_stations)
+}
+
+#' Title Convenient wrapper for filtering bird species in BBS data.
+#'
+#' @param bbs_dat Dataframe with BBS data.
+#' @param active_birds Character vector of species codes to retain.
+#'
+#' @return Dataframe filtered to only include active birds.
+#' @export
+#'
+filter_birds <- function(bbs_dat, active_birds) {
+  bbs_dat %>% filter(.data$Species_Code %in% active_birds)
+}
+
